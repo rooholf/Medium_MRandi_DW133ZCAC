6d93d629d2065d3cf7f22e883aae840ae5cf1417
Muhammad Randi <rooholf@gmail.com>
2019-12-07 21:20:46 +0700

    1.register styling

--- src/Component/Signin.js
+++ src/Component/Signin.js
@@ -3,9 +3,44 @@ import React, { Component } from 'react'
 export class Signin extends Component {
     render() {
         return (
-            <div>
-                
-            </div>
+            <Modal trigger={<Button>Login</Button>} centered={false}>
+            <Grid
+              columns={3}
+              stackable
+              style={{ height: "100vh" }}
+              verticalAlign="top"
+            >
+              <GridRow>
+                <Grid.Column>
+                <Image src="/logoLeft.png" />
+                </Grid.Column>
+                <Grid.Column textAlign="center" verticalAlign="middle" >
+                  <Segment>
+                    <Modal.Content >
+                      <Modal.Description>
+                        <Header>Join Medium</Header>
+                        <p >
+                          Create an account to receive great stories in your inbox,
+                          personalize your homepage, and follow authors and topics that
+                          you love.
+                        </p>
+                      </Modal.Description>
+                      <LoginForm />
+                    </Modal.Content>
+                  </Segment>
+                  <p>Already have an account?  <a href='/Signin'>Sign In</a></p>
+                  <p>
+                    To make Medium work, we log user data and share it with service
+                    providers. Click “Registration” above to accept Medium’s Terms of Service
+                    & Privacy Policy.
+                  </p>
+                </Grid.Column>
+                <Grid.Column >
+                <Image src="/logoRight.png" style={{position: 'absolute', right: "1vw"}}/>
+              </Grid.Column>
+              </GridRow>
+            </Grid>
+          </Modal>
         )
     }
 }
